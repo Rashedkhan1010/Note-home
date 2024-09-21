6d622a4d576b20a897d99fd8719fd5647950f262
Rashed <rashedkhanafg124@gmail.com>
2024-09-21 16:32:17 +0430

    Q3

--- Assignment-13.js
+++ Assignment-13.js
@@ -5,9 +5,25 @@ for (var i = 1; i <= 10; i++) {
 //Q2
 function isEvenOrOdd(num) {
     if (typeof num !== 'number') {
-     return 
+     return ;
     }
     return num % 2 === 0 ?
     'Even' : 'Odd'
 }
 console.log(isEvenOrOdd(3));
+//Q3
+function Vowel(str) {
+    if (typeof str !== 'string') {
+       return ;
+    }
+    const Vowel = 'aeiouAEIOU';
+    var count = 0;
+    for (var char of str) {
+        if
+        (Vowel.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log("HELOOAhmad");
